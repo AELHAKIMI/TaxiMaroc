7355f7ae2a9b019b0d25a27ed497fb4e742dc913
AELHAKIMI <ayo-voi@hotmail.com>
2018-10-22 14:20:02 +0100

    Work 22/10/2018

--- permissions/forms.py
+++ permissions/forms.py
@@ -17,9 +17,11 @@ class PermissionForm(ModelForm):
         fields = '__all__'
         widgets = {
             'permission_id' : forms.TextInput(attrs={'class': 'form-control'}),
+            'chauffeur'     : forms.TextInput(attrs={'class':  'form-control'}),
+            'taxi'          : forms.TextInput(attrs={'class': 'form-control'}),
+            'date_debut'    : forms.DateInput(attrs={'class': 'datepicker form-control'}),
+            'date_fin'      : forms.DateInput(attrs={'class': 'datepicker form-control'}),
             'destination'   : forms.TextInput(attrs={'class':'form-control'}),
-            'date_debut'    : forms.DateInput(attrs={'class': 'datepicker'}),
-            'chauffeur'     : forms.Select(attrs={'class': 'selectpicker'})
-        }
+            }
     
 
